f1bc195903600d33325d8961d6f57aa99add0e6d
Rob Sawyer <robksawyer@gmail.com>
2016-12-30 00:14:44 -0800

    Added some assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,16 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
+
+      // TODO: Fix?
+      // https://github.com/apache/cordova-plugin-screen-orientation
+      // window.screen.lockOrientation('landscape');
+      // Or?
+      // https://ionicframework.com/docs/v2/api/platform/Platform/
+      if(platform.isPortrait()){
+        //Present an alert or some dialog to let the user know to rotate.
+      }
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
